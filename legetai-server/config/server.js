5b5892baa251baf82aa022248d87f938f5f88a27
Andrew <andrew@kencolabel.com>
2021-04-18 20:44:35 -0500

    v

---
+++ legetai-server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '787cd264e00916c336e201759105d39b'),
+    },
+  },
+});
---
+++ legetai-server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'be9e445e-da58-40ff-9e6e-e344dfd1c783'
+};
